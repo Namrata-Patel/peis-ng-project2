18033923ba698f092b295edfc5e90a88a3f4af02
msnamratapatel@gmail.com <msnamratapatel@gmail.com>
2019-12-19 17:29:53 +0530

    Project 2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { View1Component } from './view1/view1.component';
 import { View2Component } from './view2/view2.component';
+
 import { NavComponent } from './nav/nav.component';
 
 // List of providers
